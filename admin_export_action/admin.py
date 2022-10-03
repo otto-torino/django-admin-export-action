25f75cd3ac66ba455d27ac3e1e402edc0e52a81e
abidibo <abidibo@gmail.com>
2022-10-03 12:36:50 +0200

    Fix ugettext import error in new django versions

--- admin_export_action/admin.py
+++ admin_export_action/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.contrib.contenttypes.models import ContentType
 from django.urls import reverse
 from django.http import HttpResponseRedirect
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from .config import get_config
 
 
